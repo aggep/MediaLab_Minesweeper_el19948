c5bfbaf29dbf2628f4dc76941e320b63ca922709
aggep <el19948@mail.ntua.gr>
2023-03-17 20:56:26 +0200

    added alerts for exceptions

--- src/main/java/com/example/medialab_minesweeper_el19948/InvalidDescriptionException.java
+++ src/main/java/com/example/medialab_minesweeper_el19948/InvalidDescriptionException.java
@@ -1,7 +1,11 @@
 package com.example.medialab_minesweeper_el19948;
 
+import javafx.scene.control.Alert;
+
 public class InvalidDescriptionException extends RuntimeException {
     public InvalidDescriptionException(String errorMsg){
         super(errorMsg);
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR, "Invalid game description.");
+        errorAlert.show();
     }
 }
--- src/main/java/com/example/medialab_minesweeper_el19948/InvalidValueException.java
+++ src/main/java/com/example/medialab_minesweeper_el19948/InvalidValueException.java
@@ -1,7 +1,11 @@
 package com.example.medialab_minesweeper_el19948;
 
+import javafx.scene.control.Alert;
+
 public class InvalidValueException extends RuntimeException {
     public InvalidValueException(String errorMessage) {
         super(errorMessage);
+        Alert errorAlert = new Alert(Alert.AlertType.ERROR, "Invalid value.");
+        errorAlert.show();
     }
 }
